06e7bb881746defe03dfae870f8227cc65deaa83
Azziko <sharipov.wdev@gmail.com>
2023-04-07 21:38:32 +0200

    hooks added and utilized

--- src/components/PostContainer/index.jsx
+++ src/components/PostContainer/index.jsx
@@ -1,15 +1,44 @@
+import dataApp from '../../data/local.json'
+
 import Post from "../Post";
+import useFetch from "../../hooks/useFetch";
 
 export default function PostContainer({className}) {
-    return(
-        <div className={"flex flex-col flex-wrap flex-start gap-4 " + className}>
-            <h4 className="text-3xl text-jet dark:text-white tracking-[0.25px] font-semibold">Recent Activity</h4>
-            <div className="posts flex flex-wrap lg:flex-col justify-between gap-y-8 lg:gap-2">
-                <Post className="animate-slideTop" name={"telegram-bot-api"} pushed_at={"2022-18-04"} description={"Lorem ipsun dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet ammet ammet"}/>
-                <Post className="animate-slideTop animation-delay-100" name={"telegram-bot-api"} pushed_at={"2022-18-04"} description={"Lorem ipsun dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet"}/>
-                <Post className="animate-slideTop animation-delay-200" name={"telegram-bot-api"} pushed_at={"2022-18-04"} description={"Lorem ipsun dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet"}/>
-                <Post className="animate-slideTop animation-delay-300" name={"telegram-bot-api"} pushed_at={"2022-18-04"} description={"Lorem ipsun dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet dolor sit ammet"}/>
+    const { data, isLoading, error } = useFetch(`https://api.github.com/users/${dataApp.gitProfileName}/repos`);
+
+    if (error) {
+        if (import.meta.env.DEV) {
+            console.log(error)
+        }
+
+        return (
+            <div className={"flex flex-col flex-wrap flex-start gap-4 " + className}>
+                <h4 className="text-3xl text-jet dark:text-white tracking-[0.25px] font-semibold">Recent Activity</h4>
+                <h3 className="text-xl text-jet dark:text-white tracking-[0.75px]  italic font-thin">{dataApp.noDataError}</h3>
             </div>
-        </div>
-    );
+        )
+    }
+
+    if (!isLoading) {
+        return(
+            <div className={"flex flex-col flex-wrap flex-start gap-4 " + className}>
+                <h4 className="text-3xl text-jet dark:text-white tracking-[0.25px] font-semibold">Recent Activity</h4>
+                <div className="posts flex flex-wrap lg:flex-col justify-between gap-y-8 lg:gap-2">
+                    {data.map((item, i) => {
+                        if (item.name != dataApp.gitProfileName && i < 5) {
+                            return  (
+                                <Post className={"animate-slideTop animation-delay-" + `${i*100}`}
+                                    key={item.id}  
+                                    name={item.name} 
+                                    pushed_at={item.pushed_at.split('T')[0]} 
+                                    description={item.description}
+                                    html_url={item.html_url}
+                                />
+                            );                    
+                        }
+                    })}
+                </div>
+            </div>
+        );
+    }
 }
