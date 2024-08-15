59ebb945594f1ac16f1619c0b12426392d83ebe1
Aziz <sharipov.wdev@gmail.com>
2024-08-15 15:49:32 +0500

    Fix display of full height for FireFox in post

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="flex min-h-[4rem] items-center border-jet dark:border-pewter border-t border-solid col-span-full">
-            <p className="font-ptSans text-sm italic text-jet dark:text-pewter">Copyright © 2023 <a className="underline" href="https://github.com/azziko">Aziz Sharipov</a></p>
+            <p className="font-ptSans text-sm italic text-jet dark:text-pewter">Page by <a className="underline" href="https://github.com/azziko">Aziz Sharipov</a></p>
         </footer>
     );
 }
